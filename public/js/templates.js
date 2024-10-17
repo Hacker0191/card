b187e2ede214861c6b2ff3b2fa054240c5d9804d
Hacker0191 <themayankgamerz@gmail.com>
2024-10-17 23:12:09 +0530

    Delete public/js/templates.js

--- public/js/templates.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.template-item').forEach(item => {
-    item.addEventListener('click', () => {
-      const templatePath = item.dataset.path;
-      window.opener.postMessage({ type: 'template-selected', templatePath }, '*');
-      window.close();
-    });
-  });
